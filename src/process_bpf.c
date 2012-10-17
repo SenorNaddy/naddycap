e2c48253bbcbbb7b5af40a79cf68d284f72a82b7
Simon Wadsworth <SenorNaddy@github>
2012-10-17 19:20:43 +1300

    Add process_bpf filter module

---
+++ src/process_bpf.c
@@ -0,0 +1,22 @@
+#include "libtrace.h"
+#include <stdio.h>
+#include "plugin.h"
+
+libtrace_filter_t *filter;
+
+void init(char *args)
+{
+	char tmp[256] = "";
+	sprintf(tmp, "icmp");
+	filter = trace_create_filter(tmp);
+}
+enum packetret parse_packet(libtrace_packet_t *pkt)
+{
+	if(trace_apply_filter(filter, pkt) == 0)
+		return DROPPED;
+	else return ACCEPTED;
+}
+void cleanup()
+{
+	trace_destroy_filter(filter);
+}
